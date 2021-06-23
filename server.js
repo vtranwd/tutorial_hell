c56b75a3278021365260f8f4230bec42d21cae68
Victor Tran <vtranwd@gmail.com>
2021-06-23 15:20:57 -0700

    configured mongoose

--- server.js
+++ server.js
@@ -26,3 +26,20 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+const app = express();
+app.use(...);
+
+const db = require("./app/models");
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
